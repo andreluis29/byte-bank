d569639ce1bd8ed3727f8ec530d2766bd1c86185
andreluis29 <andre.wenningkamp1@gmail.com>
2022-05-27 23:03:04 -0300

    Parte 3 de entendendo objetos concluída

--- src/Conta.java
+++ src/Conta.java
@@ -3,4 +3,25 @@ public class Conta {
     int agencia = 31;
     int numero;
     String titular;
+
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean transfere(double valor, Conta conta){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            conta.deposita(valor);    
+            return true;
+        }
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 200;
+        primeiraConta.deposita(200);
 
         Conta segundaConta = new Conta();
-        segundaConta.saldo = 50;
+        segundaConta.deposita(50);
 
         System.out.println("A primeira conta tem: R$" + primeiraConta.saldo);
         System.out.println("A segunda conta tem: R$" + segundaConta.saldo);
         System.out.println("ag conta 1:" + primeiraConta.agencia);
         System.out.println("ag conta 2:" + segundaConta.agencia);
+
+        primeiraConta.transfere(40, segundaConta);
+
+        System.out.println("Após transferência a segunda conta tem: R$" + segundaConta.saldo);
 
     }
 }
