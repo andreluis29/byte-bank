18a04e331d3f4373994a2619422c396f49f8dd08
andreluis29 <andre.wenningkamp1@gmail.com>
2022-05-28 00:14:56 -0300

    Pequeno teste realizado

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,59 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
         Cliente andre = new Cliente();
         andre.nome = "André Luís";
         andre.cpf = "777.157.069-51";
         andre.profissao = "Programador";
 
-        Conta primeiraConta = new Conta();
-        primeiraConta.titular = andre;
-        primeiraConta.deposita(200);
+        Cliente orlei = new Cliente();
+        orlei.nome = "Orlei Luís";
+        orlei.cpf = "777.157.069-51";
+        orlei.profissao = "Trader";
+
+        Conta contaAndre = new Conta();
+        contaAndre.titular = andre;
+        contaAndre.deposita(5000);
 
 
         System.out.println("Informações da conta\n" 
         + andre.informacoesDeCliente(andre.nome, andre.cpf, andre.profissao));
         
-        System.out.println("ag conta 1:" + primeiraConta.agencia);
-        System.out.println("saldo: R$" + primeiraConta.saldo);
+        System.out.println("ag conta:" + contaAndre.agencia);
+        System.out.println("saldo: R$" + contaAndre.saldo);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+        Conta contaOrlei = new Conta();
+        contaOrlei.titular = orlei;
+        contaOrlei.deposita(5000);
+
+
+        System.out.println("Informações da conta\n" 
+        + orlei.informacoesDeCliente(orlei.nome, orlei.cpf, orlei.profissao));
+        
+        System.out.println("ag conta:" + contaOrlei.agencia);
+        System.out.println("saldo: R$" + contaOrlei.saldo);
  
+        System.out.println("");
+        System.out.println("");
+        contaAndre.transfere(300, contaOrlei);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("novo saldo de " + orlei.nome + ": R$" + contaOrlei.saldo);
+
 
     }
 }
